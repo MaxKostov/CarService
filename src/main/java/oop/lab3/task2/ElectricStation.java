0d23e1bccbda47e71c63f74e256409bc44330249
MaxKostov <max.kostov04@gmail.com>
2024-12-01 22:58:19 +0200

    Implemented different tests for task1 and task2

--- src/main/java/oop/lab3/task2/ElectricStation.java
+++ src/main/java/oop/lab3/task2/ElectricStation.java
@@ -1,7 +1,7 @@
 package oop.lab3.task2;
 
 public class ElectricStation implements  IRefuelable {
-    public static int electricCars = 0;
+    private static int electricCars = 0;
     @Override
     public void refuel(String CarID) {
         System.out.println("Refueling electric car " + CarID + ".");
